cee4b4323e9b36e865dd5f1e24cbd660a2d0d862
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-03-20 11:51:47 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,30 +1,39 @@
 import os
 import sys
-import collections
 
 
-def find_file_names(file_path):
-    file_names = []
-    for root, _, files in os.walk(file_path):
-        file_names.extend(list(map(lambda name_of_file: (
+def find_file_names_and_sizes(file_path):
+    file_names_and_sizes_dict = dict()
+    for root, _, name_of_files in os.walk(file_path):
+        file_names_and_sizes = list(map(lambda name_of_file: (
             name_of_file,
-            os.path.getsize(root + '/' + name_of_file)), files))
+            os.path.getsize(os.path.join(root, name_of_file))), name_of_files)
                           )
-    return file_names
+        for name_and_size_of_file in file_names_and_sizes:
+            name_of_file, _ = name_and_size_of_file
+            path = os.path.join(root, name_of_file)
+            if name_and_size_of_file in file_names_and_sizes_dict:
+                file_names_and_sizes_dict[name_and_size_of_file].append(path)
+            else:
+                file_names_and_sizes_dict[name_and_size_of_file] = [path]
+    return file_names_and_sizes_dict
 
 
-def find_dublicates(file_names):
-    counter = collections.Counter(file_names)
-    dublicates = set(file[0] for file in counter if counter[file] > 1)
-    return dublicates
+def find_duplicates(file_names_and_sizes_dict):
+    duplicates = set(
+        name_of_file for name_of_file in file_names_and_sizes_dict
+        if len(file_names_and_sizes_dict[name_of_file]) > 1
+    )
+    return duplicates
 
 
 if __name__ == '__main__':
     file_path = sys.argv[1]
-    file_names = find_file_names(file_path)
-    dublicates = find_dublicates(file_names)
-    if dublicates:
-        print('Количество дубликатов:', len(dublicates))
-        print(*(file_name for file_name in dublicates), sep='\n')
+    file_names_and_sizes_dict = find_file_names_and_sizes(file_path)
+    duplicates = find_duplicates(file_names_and_sizes_dict)
+    if duplicates:
+        print('Количество дубликатов:', len(duplicates))
+        for file_name_and_size in duplicates:
+            print(*file_names_and_sizes_dict[file_name_and_size])
     else:
         print('Дубликатов нет')
